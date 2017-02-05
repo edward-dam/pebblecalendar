2d9a898274342a6daab821226e54383b06bfcbc4
Edward Dam <edward.dam@gmail.com>
2017-02-05 12:14:42 +0000

    Re-designed for v2.0

--- src/c/main.c
+++ src/c/main.c
@@ -26,7 +26,7 @@ bool date_bool;
 bool degree_bool;
 bool logo_bool;
 
-// saved time/date
+// saved date/time
 static char hour12_buffer[3];
 static char hour24_buffer[3];
 static char datedd_buffer[3];
@@ -44,9 +44,9 @@ static char temp_fah_buffer[6];
 static void load_options() {
   // load 24 or 12 hours
   if (persist_exists(hour_setting)) {
-    char buffer[5];
-    persist_read_string(hour_setting, buffer, sizeof(buffer));
-    if (strcmp(buffer, "true") == 0) {
+    char hour_buffer[5];
+    persist_read_string(hour_setting, hour_buffer, sizeof(hour_buffer));
+    if (strcmp(hour_buffer, "true") == 0) {
       hour_bool = true;
       text_layer_set_text(hour_layer, hour12_buffer);
     } else {
@@ -93,7 +93,7 @@ static void load_options() {
   } else {
     degree_bool = false;
   }
-  
+
   // load location or logo
   if (persist_exists(logo_setting)) {
     char logo_buffer[5];
@@ -119,6 +119,8 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   Tuple *date_tuple   = dict_find(iterator, MESSAGE_KEY_DATE);
   Tuple *degree_tuple = dict_find(iterator, MESSAGE_KEY_DEGREE);
   Tuple *logo_tuple   = dict_find(iterator, MESSAGE_KEY_LOGO);
+
+  // save options
   if(hour_tuple && date_tuple && degree_tuple && logo_tuple) {
     char *hour_string   = hour_tuple->value->cstring;
     char *date_string   = date_tuple->value->cstring;
@@ -136,16 +138,20 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   Tuple *weather_tuple  = dict_find(iterator, MESSAGE_KEY_WEATHER);
   Tuple *temp_cel_tuple = dict_find(iterator, MESSAGE_KEY_TEMP_CEL);
   Tuple *temp_fah_tuple = dict_find(iterator, MESSAGE_KEY_TEMP_FAH);
+
+  // display location
   if (location_tuple) {
     snprintf(location_buffer, sizeof(location_buffer), "%s", location_tuple->value->cstring);
     if (!logo_bool) {
       text_layer_set_text(logo_layer, location_buffer);
     }
-  } 
+  }
+  // display weather
   if (weather_tuple) {
     snprintf(weather_buffer, sizeof(weather_buffer), "%s", weather_tuple->value->cstring);
     text_layer_set_text(weather_layer, weather_buffer);
   }
+  // display temperature
   if (temp_cel_tuple && temp_fah_tuple) {
     snprintf(temp_fah_buffer, sizeof(temp_fah_buffer), "%d°", (int)temp_fah_tuple->value->int32);
     snprintf(temp_cel_buffer, sizeof(temp_cel_buffer), "%d°", (int)temp_cel_tuple->value->int32);
@@ -157,79 +163,48 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   }
 }
 
-// update year
-static void update_year() {
+// update date/time
+static void update_datetime() {
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
+  
+  // save date/time
+  static char mins_buffer[3];
+  static char day_buffer[10];
   static char year_buffer[5];
-  strftime(year_buffer, sizeof(year_buffer), "%Y", tick_time);
-  text_layer_set_text(year_layer, year_buffer);
-}
-static void year_tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_year();
-}
-
-// update month
-static void update_month() {
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
+  
+  // define date/time
+  strftime(mins_buffer,    sizeof(mins_buffer),    "%M", tick_time);
+  strftime(hour24_buffer,  sizeof(hour24_buffer),  "%H", tick_time);
+  strftime(hour12_buffer,  sizeof(hour12_buffer),  "%I", tick_time);
+  strftime(day_buffer,     sizeof(day_buffer),     "%A", tick_time);
+  strftime(datedd_buffer,  sizeof(datedd_buffer),  "%d", tick_time);
+  strftime(datemm_buffer,  sizeof(datemm_buffer),  "%m", tick_time);
   strftime(monthmm_buffer, sizeof(monthmm_buffer), "%m", tick_time);
   strftime(monthdd_buffer, sizeof(monthdd_buffer), "%d", tick_time);
-  if (!date_bool) {
-    text_layer_set_text(month_layer, monthmm_buffer);
-  } else {
-    text_layer_set_text(month_layer, monthdd_buffer);
-  }
-}
-static void month_tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_month();
-}
+  strftime(year_buffer,    sizeof(year_buffer),    "%Y", tick_time);
 
-// update day
-static void update_day() {
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-  static char day_buffer[10];
-  strftime(day_buffer,    sizeof(day_buffer),    "%A", tick_time);
-  strftime(datedd_buffer, sizeof(datedd_buffer), "%d", tick_time);
-  strftime(datemm_buffer, sizeof(datemm_buffer), "%m", tick_time);
-  text_layer_set_text(day_layer,  day_buffer);
-  if (!date_bool) {
-    text_layer_set_text(date_layer, datedd_buffer);
-  } else {
-    text_layer_set_text(date_layer, datemm_buffer);
-  }
-}
-static void day_tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_day();
-}
-
-// update hour
-static void update_hour() {
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-  strftime(hour24_buffer, sizeof(hour24_buffer), "%H", tick_time);
-  strftime(hour12_buffer, sizeof(hour12_buffer), "%I", tick_time);
+  // display time
+  text_layer_set_text(mins_layer, mins_buffer);
   if (!hour_bool) {
     text_layer_set_text(hour_layer, hour24_buffer);
   } else {
     text_layer_set_text(hour_layer, hour12_buffer);
-  } 
-}
-static void hour_tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_hour();
-}
+  }
 
-// update mins
-static void update_mins() {
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-  static char mins_buffer[3];
-  strftime(mins_buffer, sizeof(mins_buffer), "%M", tick_time);
-  text_layer_set_text(mins_layer, mins_buffer);
+  // display date
+  text_layer_set_text(day_layer,  day_buffer);
+  if (!date_bool) {
+    text_layer_set_text(date_layer,  datedd_buffer);
+    text_layer_set_text(month_layer, monthmm_buffer);
+  } else {
+    text_layer_set_text(date_layer,  datemm_buffer);
+    text_layer_set_text(month_layer, monthdd_buffer);
+  }
+  text_layer_set_text(year_layer, year_buffer);
 }
 static void mins_tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_mins();
+  update_datetime();
 }
 
 // drawing canvas
@@ -246,11 +221,11 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
  
   // draw boxes
   GRect logo_square = GRect(cx-65,cy-78,130,18);
-  GRect day_square  = GRect(cx-65,cy-55,85, 25);
+  GRect day_square  = GRect(cx-65,cy-55,88, 25);
   GRect batt_header = GRect(cx+30,cy-50,10, 15);
   GRect batt_square = GRect(cx+35,cy-55,30, 25);
-  GRect hour_square = GRect(cx-65,cy-25,60, 50);
-  GRect mins_square = GRect(cx +5,cy-25,60, 50);
+  GRect hour_square = GRect(cx-65,cy-20,60, 45);
+  GRect mins_square = GRect(cx +5,cy-20,60, 45);
   GRect date_square = GRect(cx-65,cy+30,30, 25);
   GRect mnth_square = GRect(cx-25,cy+30,30, 25);
   GRect year_square = GRect(cx+15,cy+30,50, 25);
@@ -278,19 +253,52 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
   graphics_fill_rect(ctx, year_square, 1, GCornersAll);
   graphics_fill_rect(ctx, weat_square, 0, GCornersAll);
   graphics_fill_rect(ctx, temp_square, 0, GCornersAll);
-  
+
   // draw dots
   graphics_context_set_fill_color(ctx, GColorBlack);
   graphics_fill_circle(ctx, GPoint(cx-58,cy-69), 4);
   graphics_fill_circle(ctx, GPoint(cx+58,cy-69), 4);
 
+  // draw calendar  
+  GRect hour_inside = GRect(cx-58,cy-10,46,30);
+  GRect mins_inside = GRect(cx+12,cy-10,46,30);
+  graphics_draw_rect(ctx, hour_inside);
+  graphics_draw_rect(ctx, mins_inside);
+  graphics_fill_rect(ctx, hour_inside, 1, GCornersAll);
+  graphics_fill_rect(ctx, mins_inside, 1, GCornersAll);
+  
+  // draw nubs
+  GRect hour_nubone = GRect(cx-55,cy-25,10,12);
+  GRect hour_nubtwo = GRect(cx-25,cy-25,10,12);
+  GRect mins_nubone = GRect(cx+15,cy-25,10,12);
+  GRect mins_nubtwo = GRect(cx+45,cy-25,10,12);
+  graphics_draw_rect(ctx, hour_nubone);
+  graphics_draw_rect(ctx, hour_nubtwo);
+  graphics_draw_rect(ctx, mins_nubone);
+  graphics_draw_rect(ctx, mins_nubtwo);
+  graphics_fill_rect(ctx, hour_nubone, 1, GCornersAll);
+  graphics_fill_rect(ctx, hour_nubtwo, 1, GCornersAll);
+  graphics_fill_rect(ctx, mins_nubone, 1, GCornersAll);
+  graphics_fill_rect(ctx, mins_nubtwo, 1, GCornersAll);
+
+  // draw nubs inside
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  GRect hour_nubin1 = GRect(cx-53,cy-25,6,10);
+  GRect hour_nubin2 = GRect(cx-23,cy-25,6,10);
+  GRect mins_nubin1 = GRect(cx+17,cy-25,6,10);
+  GRect mins_nubin2 = GRect(cx+47,cy-25,6,10);
+  graphics_draw_rect(ctx, hour_nubin1);
+  graphics_draw_rect(ctx, hour_nubin2);
+  graphics_draw_rect(ctx, mins_nubin1);
+  graphics_draw_rect(ctx, mins_nubin2);
+  graphics_fill_rect(ctx, hour_nubin1, 0, GCornersAll);
+  graphics_fill_rect(ctx, hour_nubin2, 0, GCornersAll);
+  graphics_fill_rect(ctx, mins_nubin1, 0, GCornersAll);
+  graphics_fill_rect(ctx, mins_nubin2, 0, GCornersAll);
+
   // draw lines
   graphics_context_set_stroke_width(ctx, 1);
-  graphics_draw_line(ctx, GPoint(0,cy-43), GPoint(92,cy-43));
   graphics_draw_line(ctx, GPoint(0,cy+42), GPoint(mx,cy+42));
-  //graphics_context_set_stroke_width(ctx, 3);
-  graphics_draw_line(ctx, GPoint(0,cy), GPoint(mx,cy));
-  graphics_draw_line(ctx, GPoint(0,cy+1), GPoint(mx,cy+1));
 }
 
 // window load
@@ -325,17 +333,19 @@ static void main_window_load(Window *window) {
   layer_add_child(window_layer, text_layer_get_layer(battery_layer));
 
   // time layers
-  hour_layer = text_layer_create(GRect(cx-63,cy-31,mx,my));
-  mins_layer = text_layer_create(GRect(cx +7,cy-31,mx,my));
+  hour_layer = text_layer_create(GRect(cx-50,cy-13,mx,my));
+  mins_layer = text_layer_create(GRect(cx+20,cy-13,mx,my));
   text_layer_set_background_color(hour_layer, GColorClear);
   text_layer_set_background_color(mins_layer, GColorClear);
-  text_layer_set_font(hour_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
-  text_layer_set_font(mins_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
+  text_layer_set_text_color(hour_layer, GColorWhite);
+  text_layer_set_text_color(mins_layer, GColorWhite);
+  text_layer_set_font(hour_layer, fonts_get_system_font(FONT_KEY_DROID_SERIF_28_BOLD));
+  text_layer_set_font(mins_layer, fonts_get_system_font(FONT_KEY_DROID_SERIF_28_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(hour_layer));
   layer_add_child(window_layer, text_layer_get_layer(mins_layer));
 
   // date layers
-  day_layer   = text_layer_create(GRect(cx-64,cy-60,85,my));
+  day_layer   = text_layer_create(GRect(cx-65,cy-60,88,my));
   date_layer  = text_layer_create(GRect(cx-60,cy+26,mx,my));
   month_layer = text_layer_create(GRect(cx-20,cy+26,mx,my));
   year_layer  = text_layer_create(GRect(cx+20,cy+26,mx,my));
@@ -354,15 +364,15 @@ static void main_window_load(Window *window) {
   layer_add_child(window_layer, text_layer_get_layer(year_layer));
   
   // weather layers
-  weather_layer      = text_layer_create(GRect(cx-64,cy+56,85,my));
-  temperature_layer  = text_layer_create(GRect(cx+31,cy+57,35,my));
-  text_layer_set_background_color(weather_layer,     GColorClear);
+  weather_layer = text_layer_create(GRect(cx-64,cy+56,85,my));
+  temperature_layer = text_layer_create(GRect(cx+31,cy+57,35,my));
+  text_layer_set_background_color(weather_layer, GColorClear);
   text_layer_set_background_color(temperature_layer, GColorClear);
-  text_layer_set_text_alignment(weather_layer,     GTextAlignmentCenter);
+  text_layer_set_text_alignment(weather_layer, GTextAlignmentCenter);
   text_layer_set_text_alignment(temperature_layer, GTextAlignmentCenter);
-  text_layer_set_font(weather_layer,     fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_font(weather_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
   text_layer_set_font(temperature_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
-  text_layer_set_text(weather_layer,     "Weather");
+  text_layer_set_text(weather_layer, "Weather");
   text_layer_set_text(temperature_layer, "?°");
   layer_add_child(window_layer, text_layer_get_layer(weather_layer));
   layer_add_child(window_layer, text_layer_get_layer(temperature_layer));
@@ -378,6 +388,7 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(date_layer);
   text_layer_destroy(mins_layer);
   text_layer_destroy(hour_layer);
+  text_layer_destroy(battery_layer);
   text_layer_destroy(day_layer);
   text_layer_destroy(logo_layer);
   // destroy canvas layer
@@ -402,16 +413,8 @@ static void init() {
   // load options
   load_options();
   
-  // update time/date
-  update_year();
-  update_month();
-  update_day();
-  update_hour();
-  update_mins();
-  tick_timer_service_subscribe(YEAR_UNIT,   year_tick_handler);
-  tick_timer_service_subscribe(MONTH_UNIT,  month_tick_handler);
-  tick_timer_service_subscribe(DAY_UNIT,    day_tick_handler);
-  tick_timer_service_subscribe(HOUR_UNIT,   hour_tick_handler);
+  // update date/time
+  update_datetime();
   tick_timer_service_subscribe(MINUTE_UNIT, mins_tick_handler);
   
   // update options/weather
